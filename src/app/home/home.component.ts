bac82cea2290c88ba2ce0493a11a279af8ce046f
Alex Harper <fintalex@mail.ru>
2018-05-17 11:17:05 +0300

    Changed style for Homve page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,42 +14,50 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
       var firstImageAndSecondSection = 462 + window.innerHeight;
 
+      var contPMG2 = $('.pimg2-container');
+
       $(window).scroll(function(){
 
-        var wScroll = $(this).scrollTop();
+        var wScroll = $(this).scrollTop();        
 
-        console.log(wScroll);
-        $('.left-fast').css({
-            'transform': 'translate(0px, ' + wScroll * 0.3 + 'px)'
-        });
+        if (contPMG2.offset().top - window.innerHeight - wScroll < 0 ){
+            var imageScroll = window.innerHeight + wScroll - contPMG2.offset().top;
 
-        $('.right-fast').css({
-            'transform': 'translate(0px, ' + wScroll * 0.4 + 'px)'
-        });
-        
-        $('.center-fix').css({
-            'transform': 'translate(0px, ' + wScroll * 0.1 + 'px)'
-        });
-        
-        $('.center-to-right').css({
-            'transform': 'translate(0px, -' + wScroll * 0.1 + 'px)'
-            //'transform': 'translate(' + wScroll * 0.2 + 'px, 0px)'
-        });  
-        
-        $('.center-to-up').css({
-            'transform': 'translate(0px, -' + wScroll * 0.2 + 'px)'
-            //'transform': 'translate(' + wScroll * 0.2 + 'px, 0px)'
-        });  
+            console.log('imageScroll', imageScroll);
+            //console.log('contPMG2.offset().top', contPMG2.offset().top);
+
+            $('.left-fast').css({
+                'transform': 'translate(0px, ' + imageScroll * 0.35 + 'px)'
+            });
+
+            $('.right-fast').css({
+                'transform': 'translate(0px, ' + imageScroll * 0.4 + 'px)'
+            });
+            
+            $('.center-fix').css({
+                'transform': 'translate(0px, ' + imageScroll * 0.1 + 'px)'
+            });
+            
+            $('.center-to-right').css({
+                'transform': 'translate(0px, -' + imageScroll * 0.1 + 'px)'
+            });  
+            
+            $('.center-to-up').css({
+                'transform': 'translate(0px, -' + imageScroll * 0.2 + 'px)'
+            });  
+
+            if (wScroll > imageScroll){
+                $('.pimg2').css({
+                    'transform': 'translate(0px, ' + (imageScroll + 200) * 0.3 + 'px)'
+                });
+            }
+        }
 
         $('.pimg1').css({
             'transform': 'translate(0px, -' + wScroll / 2 + 'px)'
         });
 
-        if (wScroll > 460){
-            $('.pimg2').css({
-                'transform': 'translate(0px, ' + (wScroll - 460) * 0.3 + 'px)'
-            });
-        }
+        
 
         if (wScroll > 2000){
             $('.pimg3').css({
